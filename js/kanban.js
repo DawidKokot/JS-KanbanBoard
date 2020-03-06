d86bc4f12ca565dbf3eb635895b5a9c51487a8d7
DawidKokot <kokot.1991@o2.pl>
2020-03-06 12:10:29 +0000

    2. item params

--- js/kanban.js
+++ js/kanban.js
@@ -6,17 +6,21 @@ const message = 'Please add a description.';
 
 const add_btn = document.querySelector('.add');
 add_btn.addEventListener('click', () => {
-  const target = document.querySelector('#requested');
-  if (adding == false) {
-    adding = true;
-    target.appendChild(create_item());
-  } else {
-    error.innerHTML = message;
-  }
+    const target = document.querySelector('#requested');
+    if (adding == false) {
+        adding = true;
+        target.appendChild(create_item());
+    } else {
+        error.innerHTML = message;
+    }
 });
 
 const create_item = () => {
   const item = document.createElement("div");
+  item.classList.add("item");
+//   item.id = `item-${order}`;
+  item.id = 'item-' + order;
+  item.draggable = true;
 };
 
 document.querySelectorAll('.drop').forEach(element => {
